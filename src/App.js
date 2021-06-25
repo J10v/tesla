db2fd2ec7bc2dad5fa08e9f185aef37285844b1f
J10v <68559232+J10v@users.noreply.github.com>
2021-06-25 13:54:11 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./Components/Headers";
+import Home from "./Components/Home"
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
